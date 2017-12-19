ec6e2656aa5318681df19e74854d4ab5d4d3c264
Alexander Solovyov <solovyov.alexander.a@gmail.com>
2017-12-19 09:42:40 -0800

    Additional task solved

--- Server/main.cpp
+++ Server/main.cpp
@@ -20,6 +20,45 @@ void parseArg(int argc, char *argv[], char *&portTCP, char *&portUDP)
     }
 }
 
+void gatherData(const char *message)
+{
+    int numbers[10] = { };
+    const char key[] = "0123456789\n";
+    --message;
+    while ((message = strpbrk(message + 1, key)) != nullptr)
+    {
+        if (!isdigit(*message))
+        {
+            break;
+        }
+        ++numbers[*message - '0'];
+    }
+    long sum = 0;
+    int minimum = 10;
+    int maximum = 0;
+    for (int i = 9; i >= 0; --i)
+    {
+        if (numbers[i])
+        {
+            minimum = std::min(minimum, i);
+            maximum = std::max(maximum, i);
+            sum += numbers[i] * i;
+
+            while (numbers[i] > 0)
+            {
+                std::cout << i << " ";
+                --numbers[i];
+            }
+        }
+    }
+    if (minimum != 10)
+    {
+        std::cout << std::endl << "Sum: " << sum << std::endl
+                  << "Min: " << minimum << std::endl
+                  << "Max: " << maximum << std::endl;
+    }
+}
+
 void processUDP(int port)
 {
     ServerUDP serverUDP;
@@ -36,6 +75,7 @@ void processUDP(int port)
         data = serverUDP.getMessage();
         if (data->getMessage(message))
         {
+            gatherData(message);
             data->sendBack(message);
             delete[] message;
         }
@@ -60,6 +100,7 @@ void processTCP(int port)
         
         while (connection->getMessage(message))
         {
+            gatherData(message);
             connection->sendMessage(message);
             delete[] message;
         }
